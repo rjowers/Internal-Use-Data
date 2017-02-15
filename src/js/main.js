11547aea41b2e14be625dee07d3a00abc62cd81d
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-15 17:08:36 -0500

    finished styling

--- src/js/main.js
+++ src/js/main.js
@@ -14,18 +14,18 @@ function getStuff(data){
   console.log(data);
   for (var count = 0; count < data.results.length; count++){
   $(".container").append(
-
-
-
-  `  <ul>
-  <li><img src="${data.results[count].picture.large}"/></li>
-  <li class="names">${data.results[count].name.first} ${data.results[count].name.last}</li>
-  <li class="emails">${data.results[count].email}</li>
-  <li class="streets">${data.results[count].location.street}</li>
-  <li class="cities">${data.results[count].location.city} ${data.results[count].location.state} ${data.results[count].location.postcode}</li>
-  <li class="phonenumbers">${data.results[count].phone}</li>
-  </ul> `
-
+    `
+  <div class="stuff">
+   <ul>
+  <div><li class="images"><img src="${data.results[count].picture.large}"/></li></div>
+  <div><li class="names"><span>${data.results[count].name.first} ${data.results[count].name.last}</span></li></div>
+  <div><li class="emails">${data.results[count].email}</li></div>
+  <div><li class="streets">${data.results[count].location.street}</li></div>
+  <div><li class="cities">${data.results[count].location.city} ${data.results[count].location.state} ${data.results[count].location.postcode}</li></div>
+  <div><li class="phonenumbers">${data.results[count].phone}</li></div>
+  </ul>
+  </div>
+  `
 )
 }
 };
